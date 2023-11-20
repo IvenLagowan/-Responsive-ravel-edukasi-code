161da22536d8d3775ac46040676044c11846ace4
Iven Lagowan <cuaocuq@gmail.com>
2023-11-20 16:44:31 +0900

    new

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # -Responsive-ravel-edukasi-code
 Perancangan website travel edukasi code menggunakan (HTML, CSS, JS, PHP &amp; MYSQL)
+
+## link tutor ('https://youtu.be/34MBVXsDOtA?feature=shared)
---
+++ home.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <!-- Link font awesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <!-- custom file link -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+    <!--  header section start -->
+    <section class="header">
+
+        <a href="home.php" class="logo"><img src="images/logo.jpg" class="logo-img">E'code Travel</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+    <!--  header section end -->
+
+
+    <!-- Footer section start -->
+    <section class="footer">
+
+        <div class="box-container">
+            <h3>Quick Links</h3>
+            <div class="box">
+                <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+                <a href="package.php"> <i class="fas fa-angle-right"></i> Package</a>
+                <a href="book.php"> <i class="fas fa-angle-right"></i> Book</a>
+            </div>
+
+            <div class="box">
+            <h3>Extra Links</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> Ask question</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> About us</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> Privacy poli</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> Terms of use</a>
+            </div>
+            <h3>Contact Info</h3>
+            <div class="box">
+                <a href="#"> <i class="fas fa-phone"></i> +62 8525 542 21251</a>
+                <a href="#"> <i class="fas fa-phone"></i> +62 8525 542 21111</a>
+                <a href="#"> <i class="fas fa-envelope"></i> cuaocuq@gmail.com</a>
+                <a href="#"> <i class="fas fa-map"></i> Kurima, Papua - 400203</a>   
+            </mdiv>
+            <div class="box">
+                <h3>Follow Us</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i> Facebook</a>
+                <a href="#"> <i class="fab fa-twitter"></i> Twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i> Instagramr</a>
+                <a href="#"> <i class="fab fa-linkedin"></i> Linkedin</a>
+            </div>
+
+            
+        </div>
+
+
+    </section>
+    <!-- Footer section end -->
+
+
+
+
+
+    <!-- custom js file link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script scr="js/script.js"></script>
+</body>
+
+</html>
